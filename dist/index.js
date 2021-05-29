bab29e5fd83c47883c2e76ffebada0659b369f3a
Arth Raval <connect.arth.me@gmail.com>
2021-05-29 20:17:21 +0530

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+});
+
